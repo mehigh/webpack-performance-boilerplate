777851dacf83176e79438b74c82d1e448cd5ce6b
Mike Crantea <mike.crantea@xwp.co>
2021-01-26 22:05:48 +0000

    Ignore Dist folder

--- dist/js/import2.js
+++
@@ -1,8 +0,0 @@
-(self.webpackChunkwebpack_esnext_boilerplate=self.webpackChunkwebpack_esnext_boilerplate||[]).push([["import2"],{"./src/js/examples/dependency-2.js":
-/*!*****************************************!*\
-  !*** ./src/js/examples/dependency-2.js ***!
-  \*****************************************/(e,s,p)=>{"use strict";p.r(s),p.d(s,{dep2:()=>r});var r="dep-2"},"./src/js/examples/import-2.js":
-/*!*************************************!*\
-  !*** ./src/js/examples/import-2.js ***!
-  \*************************************/(e,s,p)=>{"use strict";p.r(s),p.d(s,{import2:()=>r});var r="imported: "+p(/*! ./dependency-2 */"./src/js/examples/dependency-2.js").dep2}}]);
-//# sourceMappingURL=import2.js.map
--- dist/js/import3.js
+++
@@ -1,5 +0,0 @@
-(self.webpackChunkwebpack_esnext_boilerplate=self.webpackChunkwebpack_esnext_boilerplate||[]).push([["import3"],{"./src/js/examples/import-3.js":
-/*!*************************************!*\
-  !*** ./src/js/examples/import-3.js ***!
-  \*************************************/(e,s,t)=>{"use strict";t.r(s),t.d(s,{default:()=>c});const c={carouselSelector:".carousel",init:function(e){e&&(this.carouselSelector=e),this.carousels=document.querySelectorAll(this.carouselSelector+":not(.active)");for(var s=0;s<this.carousels.length;s++)this.carousels[s].classList.add("active")}}}}]);
-//# sourceMappingURL=import3.js.map
